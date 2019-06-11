fba06536d7b5beb556d1d9d0dcd847106c04854b
Aleksandra Ponochovna <47608387+AleksandraPonochovna@users.noreply.github.com>
2019-06-11 08:58:49 +0300

    Update LinkedListApp.java

--- LinkedListApp.java
+++ LinkedListApp.java
@@ -1,1 +1,18 @@
+package com.company;
 
+public class LinkedListApp {
+
+    public static void main(String[] args) {
+        MyLinkedList<String> myLinkedList = new MyLinkedList<>();
+        myLinkedList.add("Hi!");
+        myLinkedList.add("World!");
+        myLinkedList.add("I'am Sasha", 1);
+        myLinkedList.add("You are beautiful!");
+        myLinkedList.add("I love you!");
+        myLinkedList.set("Beautiful World!", 1);
+        System.out.println(myLinkedList.get(2));
+        System.out.println(myLinkedList.isEmpty());
+        System.out.println(myLinkedList.remove("Hi!"));
+        System.out.println(myLinkedList.remove(0));
+    }
+}
